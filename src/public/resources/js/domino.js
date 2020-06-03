f095a1ea6c3d8e2a6fddfacb908aa34037a35b63
Gabor Mate <mate_gabor@live.com>
2020-06-03 18:33:05 -0400

    A slighlty improved UI

--- src/public/resources/js/domino.js
+++ src/public/resources/js/domino.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
 
-
 $("#start-game-buttton").click(
 	function () {
+		
 		var numOfPlayers = $("#number-player-input").val();
+		
+		// Check Input
 		if (numOfPlayers < 2 || numOfPlayers > 4) {
-			$("#input-alert").show();
-			$( "#result" ).empty();
+			
+			// Show error message
+			$("#number-player-input-error").show();
+			$("#game-result").empty();
+			
 		} else {
-			$("#input-alert").hide();
+			// Process game and get result
+			$("#number-player-input-error").hide();
 			
 			var ajaxPost = $.post("app/domino.php", {'numPlayers': numOfPlayers });
 			
@@ -16,27 +22,27 @@ $("#start-game-buttton").click(
 				data = $.parseJSON( data );
 				
 				// Inital setup
-				$( "#progress" ).empty();
+				$("#progress").empty();
 				$.each(data.progress.start.hands, function (index, value){
 					index++;
-					$( "#progress" ).append( "<p>Player #" + index + " starting hand: <span style='font-size: 3rem'>" + value.join('')  + "</span></p>");
+					$("#progress").append( "<p>Player #" + index + " starting hand: <span style='font-size: 3rem'>" + value.join('')  + "</span></p>");
 				});
-				$( "#progress" ).append("<h2>First tile on the table</h2><p><span style='font-size: 3rem'>"+data.progress.start.table+"</span></p>");
-				$( "#progress" ).append("<h1>Progress</h1>");
+				$("#progress").append("<h4>First tile on the table</h4><p><span style='font-size: 3rem'>"+data.progress.start.table+"</span></p>");
+				
+				// Progress
+				$("#progress").append("<h4>Progress</h4>");
 				$.each(data.progress.game, function (index, value){
 					index++;
-					$( "#progress" ).append( "<p>"+value+"</p>");
+					$("#progress").append( "<p>"+value+"</p>");
 				});
 
-		    $( "#result" ).empty().append( data.winner );
-		    $( "#played-tiles" ).empty().append( data.playedTiles );
+				// Results
+		    $("#result").empty().append( data.winner );
+		    $("#played-tiles").empty().append( data.playedTiles );
 		  });
 			
 		}
 	}	
 );
-
-
-
 
 });
